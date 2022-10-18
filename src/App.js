15f450ac80f7d8d5285d9d92de05e717876723e0
Kokate0307Rutuja <rutujakokate2000@gmail>
2022-10-18 11:13:01 +0530

    initial setup for project completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+//react silk css
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+    return ( <
+        div >
+        <
+        h1 className = "text-3xl font-bold underline" > Hello world! < /h1>{" "} <
+        /div>
+    );
+}
+
+export default App;
